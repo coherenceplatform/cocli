3e5a07dc358e816fc3596cccabc02253926fb1cc
Adam Abdelaziz <adam.abdelaziz@withcoherence.com>
2023-11-09 17:12:15 -0500

    Multi arch build

---
+++ cocli_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+package_name=$1
+if [[ -z "$package_name" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+
+platforms=("darwin/arm64" "linux/arm64" "windows/amd64")
+
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name=$package_name'-'$GOOS'-'$GOARCH
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi
+
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name main.go
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
