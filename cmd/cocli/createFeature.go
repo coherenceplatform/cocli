910681ed415b6f44b51c15af8bb4f42f83aea4ce
Adam Abdelaziz <adam.abdelaziz@withcoherence.com>
2023-11-09 15:24:39 -0500

    Add commit_sha flag to feature create command

--- cmd/cocli/createFeature.go
+++ cmd/cocli/createFeature.go
@@ -11,6 +11,8 @@ import (
 )
 
 var createFeatureAppId int
+var createFeatureCommitSha string
+var createFeatureName string
 
 var createFeatureCmd = &cobra.Command{
 	Use:   "create <branch_name>",
@@ -18,9 +20,15 @@ var createFeatureCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		branchName := args[0]
+		if createFeatureName == "" {
+			createFeatureName = branchName
+		}
+
 		createFeaturePayloadData := map[string]string{
 			"application_id": fmt.Sprint(createFeatureAppId),
 			"branch_name":    branchName,
+			"commit_sha":     createFeatureCommitSha,
+			"name":           createFeatureName,
 		}
 		payloadBytes, err := json.Marshal(createFeaturePayloadData)
 		if err != nil {
@@ -54,4 +62,6 @@ var createFeatureCmd = &cobra.Command{
 func init() {
 	createFeatureCmd.Flags().IntVarP(&createFeatureAppId, "app_id", "a", 0, "App ID (required)")
 	createFeatureCmd.MarkFlagRequired("app_id")
+	createFeatureCmd.Flags().StringVarP(&createFeatureCommitSha, "commit_sha", "c", "", "Commit SHA (optional)")
+	createFeatureCmd.Flags().StringVarP(&createFeatureName, "name", "n", "", "Feature name (optional - defaults to branch_name)")
 }
