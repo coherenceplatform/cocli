c3a35276048ff752bfea48b41981384221d2823c
Adam Abdelaziz <adam.abdelaziz@withcoherence.com>
2024-07-22 23:07:42 +0000

    Update cnc wrapper command
    Add handling for sigint to cnc wrapper command

--- cmd/cocli/cnc.go
+++ cmd/cocli/cnc.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/coherenceplatform/cocli/pkg/cocli"
 	"github.com/spf13/cobra"
@@ -77,21 +80,49 @@ var cncCmd = &cobra.Command{
 			fmt.Sprintf("CNC_ENVIRONMENTS_PATH=%s", outputFile),
 		)
 
-		// Run a shell command using the arguments passed to the Cobra command
-		cmdExec := exec.Command("cnc", args...)
-		cmdExec.Stdin = os.Stdin
-		cmdExec.Stdout = os.Stdout
-		cmdExec.Stderr = os.Stderr
-		cmdExec.Env = env
+		// Create a channel to receive OS signals
+		sigChan := make(chan os.Signal, 1)
+		// Notify the channel for SIGINT (Ctrl+C)
+		signal.Notify(sigChan, syscall.SIGINT)
 
-		if err := cmdExec.Run(); err != nil {
-			fmt.Printf("Error running command: %v\n", err)
-			os.Exit(1)
-		}
+		// Create a channel to signal command completion
+		done := make(chan bool, 1)
 
-		if err := os.Remove(outputFile); err != nil {
-			fmt.Printf("Error deleting file %s: %v\n", outputFile, err)
-			os.Exit(1)
+		go func() {
+			// Run a shell command using the arguments passed to the Cobra command
+			cmdExec := exec.Command("cnc", args...)
+			cmdExec.Stdin = os.Stdin
+			cmdExec.Stdout = os.Stdout
+			cmdExec.Stderr = os.Stderr
+			cmdExec.Env = env
+
+			if err := cmdExec.Run(); err != nil {
+				fmt.Printf("Error running command: %v\n", err)
+				os.Exit(1)
+			}
+
+			done <- true
+		}()
+
+		// Wait for either the command to finish or an interrupt
+		select {
+		case <-done:
+			// Command completed normally
+			if err := os.Remove(outputFile); err != nil {
+				fmt.Printf("Error deleting file %s: %v\n", outputFile, err)
+				os.Exit(1)
+			}
+			os.Exit(0)
+		case <-sigChan:
+			fmt.Println("\nInterrupt received. Waiting for cleanup...")
+			time.Sleep(3 * time.Second)  // Adjust this duration as needed
+			fmt.Println("Exiting.")
+
+			if err := os.Remove(outputFile); err != nil {
+				fmt.Printf("Error deleting file %s: %v\n", outputFile, err)
+				os.Exit(1)
+			}
+			os.Exit(0)
 		}
 	},
 }
